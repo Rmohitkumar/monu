9d0e228602cf14ca480dec47f709c750bdfcf8b8
Mohit Kumar <its2002mohit@gmail.com>
2021-08-17 19:42:01 +0530

    Create start.cpp

---
+++ start.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int ways(int n, int m , int x, int y) {
+     if (x == n - 1 && y == m - 1) return 1;
+     if (x >= n || y >= m)  return 0;
+     return ways(n, m , x + 1, y) + ways(n, m , x, y + 1);
+}
+
+int main()
+{
+
+     cout << ways(2, 1, 0, 0) << endl;
+     return 0;
+}
